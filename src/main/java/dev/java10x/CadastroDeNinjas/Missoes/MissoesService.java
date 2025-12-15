edf23e41472dc621bd71c2c8df1ea9903e1d2129
Ademar Neto <ademarnetodev@gmail.com>
2025-12-15 16:01:45 -0300

    feat: adiciona MissoesService com operações de CRUD

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,57 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class MissoesService {
+
+    private final MissoesRepository missoesRepository;
+    private final MissoesMapper missoesMapper;
+
+    public MissoesService(MissoesRepository missoesRepository, MissoesMapper missoesMapper) {
+        this.missoesRepository = missoesRepository;
+        this.missoesMapper = missoesMapper;
+    }
+
+    // Listar todas as missões
+    public List<MissoesDTO> listarMissoes(){
+        List<MissoesModel> missoes = missoesRepository.findAll();
+        return missoes.stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    // Listar missão por id
+    public MissoesDTO listarMissoesPorId(Long id){
+        Optional<MissoesModel> missaoPorId = missoesRepository.findById(id);
+        return missaoPorId.map(missoesMapper::map).orElse(null);
+    }
+
+    //Criar missão
+    public MissoesDTO criarMissao(MissoesDTO dto){
+        MissoesModel model = missoesMapper.map(dto);
+        model = missoesRepository.save(model);
+        return missoesMapper.map(model);
+    }
+
+    // Atualizar missão
+    public MissoesDTO atualizarMissao(Long id, MissoesDTO dto) {
+        Optional<MissoesModel> missaoExistente = missoesRepository.findById(id);
+        if (missaoExistente.isPresent()) {
+            MissoesModel missaoAtualizada = missoesMapper.map(dto);
+            missaoAtualizada.setId(id);
+            MissoesModel salva = missoesRepository.save(missaoAtualizada);
+            return missoesMapper.map(salva);
+        }
+        return null;
+    }
+
+        // Deletar missão
+        public void deletarMissaoPorId(Long id) {
+            missoesRepository.deleteById(id);
+        }
 }
